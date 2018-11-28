dd258fe66001bf026309b6e9a53c2c32efef6151
Zander Dyer <zanderdyer@Zanders-MacBook-Pro.local>
2018-11-27 17:54:25 -0700

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,39 @@
 //create on click event when img is clicked
 $(".crystal-image").on("click", function () {
+    
     alert("You clicked a crystal!");
-});
 
-//create counbter to track total
+});
+console.log('TEST');
+
+//create counter to track total
 var counter = 0;
 $(".crystal-image").on("click", function () {
+    
     counter += 1;
+    
     alert("This crystal has been clicked!" + counter + "times!");
-})
+});
+console.log('TEST2');
+
+var counter = 0;
+$(".crystal-image").on("click", function () {
+    
+    counter += 10;
+    
+    alert("New score is: " + couner);
+});
+
+//set target number to guess 
+var targetNumber = 45;
+$("#numberToGuess").text(targetNumber);
+
+var counter = 0;
+
+$(".crystal-image".on("click", function () {
+    
+    counter += 10;
+    
+    alert("New score is: " + counter);
+
+})); 
