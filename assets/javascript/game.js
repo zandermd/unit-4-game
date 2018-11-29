251d7c849ad836115d2d01470cccfd2de02e2488
Zander Dyer <zanderdyer@Zanders-MacBook-Pro.local>
2018-11-29 11:47:10 -0700

    updates to JS and CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 //create on click event when img is clicked
-$(".crystal-image").on("click", function () {
-    
+$(".buttons").on("click", function () {
+
     alert("You clicked a crystal!");
 
 });
@@ -8,32 +8,67 @@ console.log('TEST');
 
 //create counter to track total
 var counter = 0;
-$(".crystal-image").on("click", function () {
-    
+$(".buttons").on("click", function () {
+
     counter += 1;
-    
-    alert("This crystal has been clicked!" + counter + "times!");
+
+    alert("This crystal has been clicked!");
+
 });
 console.log('TEST2');
 
 var counter = 0;
-$(".crystal-image").on("click", function () {
-    
+$(".buttons").on("click", function () {
+
     counter += 10;
-    
-    alert("New score is: " + couner);
+
+    alert("New score is: " + counter);
 });
 
 //set target number to guess 
-var targetNumber = 45;
+var targetNumber = 66;
 $("#numberToGuess").text(targetNumber);
 
 var counter = 0;
+var numOptions = [10, 6, 4, 11];
+var increment = numOptions[Math.round(Math.random())];
 
-$(".crystal-image".on("click", function () {
-    
+$(".buttons").on("click", function () {
+
     counter += 10;
-    
+
     alert("New score is: " + counter);
 
-})); 
+    //if statement to see if the counter is mathching the targetNumber. this is an event, with each click the numbers get compared against the targetNumber.
+    if (counter === targetNumber) {
+        alert("You win!");
+    }
+
+    //if the users clicks dont match the total they will get the loose alert
+    else if (counter >= targetNumber) {
+        alert("You lose!");
+    }
+
+});
+
+for (var i = 0; i < numOptions.length; i++) {
+    var image = $("<img>");
+    image.addClass("buttons");
+    image.attr("src", "assets/images/crystal.jpeg");
+    image.attr("data-imagevalue", numOptions[i]);
+    $(".buttons").append(image);
+}
+
+$(".buttons").on("click", function () {
+    var imagevalue = ($(this).attr("data-imagevalue"));
+    imagevalue = parseInt(imagevalue);
+    counter += imagevalue;
+    alert("New Score: " + counter);
+    if (counter === targetNumber) {
+        alert("You win!");
+    }
+    else if (counter >= targetNumber) {
+        alert("You loose!");
+    }
+});
+
